54e62f86a57b9bc0b4b336c9fdfc076f2fb914e0
zmzm1105 <75598449+mkzm1012@users.noreply.github.com>
2024-04-20 20:19:12 +0800

    Update schedulertest.c

--- user/schedulertest.c
+++ user/schedulertest.c
@@ -2,47 +2,29 @@
 #include "library/stdio.h"
 #include "library/errno.h"
 
-int create_process(const char *exec, int priority)
-{
+int create_process(const char *exec, int priority){
     int pfd = syscall_open_file(KNO_STDDIR, exec, 0, 0);
-
-    if (pfd >= 0)
-    {
+    if (pfd >= 0){
         int pid = syscall_process_prun(pfd, 0, &exec, priority);
-        if (pid > 0)
-        {
+        if (pid > 0){
             printf("created %s with priority %d\n", exec, priority);
-        }
-        else
-        {
+        }else{
             printf("couldn't run %s: %s\n", exec, strerror(pid));
         }
         syscall_object_close(pfd);
     }
-    else
-    {
+    else{
         printf("couldn't find %s: %s\n", exec, strerror(pfd));
     }
     return 0;
 }
 
-int main(int argc, char const *argv[])
-{
-    // number of processes
-    int n = 5;
-
-    // change add processes here
-    const char *processes[] = {"bin/process1.exe", "bin/process2.exe", "bin/process3.exe", "bin/process4.exe", "bin/process5.exe"};
-
-    // change add/change priorities here
-    int priorities[] = {9, 7, 2, 1, 5};
-
-    // iteratively create processes
-    for (int i = 0; i < n; i++)
-    {
-        create_process(processes[i], priorities[i]);
-    }
-    // run all processes
+int main(int argc, char const *argv[]){
+    create_process("bin/process1.exe", 9);
+    create_process("bin/process2.exe", 7);
+    create_process("bin/process3.exe", 2);
+    create_process("bin/process4.exe", 1);
+    create_process("bin/process5.exe", 5);
     syscall_run_all();
     return 0;
 }
