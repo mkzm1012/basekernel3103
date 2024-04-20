96f67480060383d8dba4ff07d08f802b7e4e7cfc
zmzm1105 <75598449+mkzm1012@users.noreply.github.com>
2024-04-20 16:00:18 +0800

    Update schedulertest.c

--- user/schedulertest.c
+++ user/schedulertest.c
@@ -2,6 +2,27 @@
 #include "library/stdio.h"
 #include "library/errno.h"
 
+//start-up program
+const char * exec = "bin/process1.exe";
+int argc;
+const char *argv[3];
+argv[0] = exec;
+argv[1] = argument1;
+argv[2] = argument2;
+int pfd = syscall_open_file(KNO_STDDIR,exec,0,0);
+
+if(pfd>=0) {
+    int pid = syscall_process_run(pfd, argc,&argv);
+    if(pid > 0) {
+        printf("started process %d\n", pid); } 
+    else {
+        printf("couldn't run %s: %s\n", argv[0],strerror(pid));
+    }
+    syscall_object_close(pfd); 
+} else {
+    printf("couldn't find %s: %s\n", argv[0],strerror(pfd));
+}
+
 int create_process(const char *exec, int priority)
 {
     int pfd = syscall_open_file(KNO_STDDIR, exec, 0, 0);
